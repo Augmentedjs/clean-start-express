ebf0b1fbb105eaa8c5eb8744d60a0f1608a9a953
karencalpo <minnkc@gmail.com>
2019-08-27 18:47:05 -0700

    Updated package.json and constants.

--- service/constants.js
+++ service/constants.js
@@ -2,8 +2,8 @@
 const port = 8080;
 const sslPort = 8443;
 const localSSLURI = `https://localhost:${sslPort}`;
-const SSLURI = `https://10.251.216.133:${sslPort}`;
-const webServiceURI = `http://10.251.216.133:${port}`;
+const SSLURI = `https://10.251.216.133:${sslPort}`; //Must add an IP address. Example: `https://10.251.216.133:${sslPort}`
+const webServiceURI = `http://10.251.216.133:${port}`; //Must add an IP address. Example: `http://10.251.216.133:${port}`
 const localWebServiceURI = `http://localhost:${port}`;
 
 module.exports.ALLOWED_ORIGINS = [localSSLURI, SSLURI, webServiceURI, localWebServiceURI];
